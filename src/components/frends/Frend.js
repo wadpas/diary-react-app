c388d3784f93de937a39ac589f99488a1f579d91
wadpas <wadpas@gmail.com>
2023-02-07 08:35:09 +0200

    implemented hook useParams

--- src/components/frends/Frend.js
+++ src/components/frends/Frend.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import persons from '../data/persons'
 
 const Frend = () => {
-  return <div>Frend</div>
+  const params = useParams()
+  const frend = persons.find((p) => p.lastName === params.lastName)
+  return (
+    <>
+      <h3>
+        {frend.lastName} {frend.firstName} - {frend.email}
+      </h3>
+      <Link to=".." relative="path">
+        Back
+      </Link>
+    </>
+  )
 }
 
 export default Frend
--- src/components/frends/Frends.js
+++ src/components/frends/Frends.js
@@ -1,5 +1,14 @@
-const Authors = () => {
-  return <div>authors</div>
+import { Link } from 'react-router-dom'
+import persons from '../data/persons'
+
+const Frends = () => {
+  return persons.map((p) => (
+    <p key={p.id}>
+      <Link to={p.lastName} className="frendLink">
+        {p.lastName} {p.lastName}
+      </Link>
+    </p>
+  ))
 }
 
-export default Authors
+export default Frends
